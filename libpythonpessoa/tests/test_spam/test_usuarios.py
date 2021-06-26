ac6c58aaf8717137ce797522e9d5a7f0f9b6d558
pessoasnil <Metroide171190y@>
2021-06-26 17:50:01 -0300

    Finalizando pytest fixture

--- libpythonpessoa/tests/test_spam/test_usuarios.py
+++ libpythonpessoa/tests/test_spam/test_usuarios.py
@@ -1,31 +1,33 @@
+import pytest
+
 from libpythonpessoa.spam.deb import Conexao
 from libpythonpessoa.spam.modelos import Usuario
 
+@pytest.fixture()
+def conexao():
+    #Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    #Tear Down
+    conexao_obj.fechar()
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+
+@pytest.fixture()
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Lins')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Lins'), Usuario(nome='Pessoa')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
-
-
-
-
-
-
