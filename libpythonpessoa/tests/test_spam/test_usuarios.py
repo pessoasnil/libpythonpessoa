bca0dbc1763c29d1c9481174f749afeb2021870b
pessoasnil <Metroide171190y@>
2021-06-26 16:22:16 -0300

    Infra basica Pytest OK

--- libpythonpessoa/tests/test_spam/test_usuarios.py
+++ libpythonpessoa/tests/test_spam/test_usuarios.py
@@ -1,32 +1,6 @@
-class Sessao:
-    contador = 0
-    usuarios=[]
-    def salvar(self, usuario):
-        Sessao.contador += 1
-        usuario.id  = Sessao.contador
-        self.usuarios.append(usuario)
-    def listar(self):
-        return self.usuarios
+from libpythonpessoa.spam.deb import Conexao
+from libpythonpessoa.spam.modelos import Usuario
 
-    def roll_back(self):
-        pass
-
-    def fechar(self):
-        pass
-
-
-class Conexao:
-    def gerar_sessao(self):
-        return Sessao()
-
-    def fechar(self):
-        pass
-
-
-class Usuario:
-    def __init__(self,nome):
-        self.nome = nome
-        self.id = None
 
 def test_salvar_usuario():
     conexao = Conexao()
@@ -42,7 +16,7 @@ def test_salvar_usuario():
 def test_listar_usuarios():
     conexao = Conexao()
     sessao = conexao.gerar_sessao()
-    usuarios = [Usuario(nome='Lins'),Usuario(nome='Pessoa')]
+    usuarios = [Usuario(nome='Lins'), Usuario(nome='Pessoa')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
