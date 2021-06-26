cef53b3d16a421761299bfdba748d5afc504da9a
pessoasnil <Metroide171190y@>
2021-06-26 15:12:35 -0300

    Implemtentando gerenciador de pacotes with e usando raise (EXCEPETION) PARA CRIAR PORCOS E DIFERENÇAS

--- libpythonpessoa/tests/test_spam/test_enviador_de_email.py
+++ libpythonpessoa/tests/test_spam/test_enviador_de_email.py
@@ -1,13 +1,17 @@
 import pytest
 
-from libpythonpessoa.spam.enviador_de_email import Enviador
+from libpythonpessoa.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-@pytest.mark.parametrize('destinatario ', ['foo@bar.com.br', 'pessoasnil@gmail.com',])
+
+@pytest.mark.parametrize(
+    'destinatario ',
+    ['foo@bar.com.br', 'pessoasnil@gmail.com', ]
+)
 def test_remetente(destinatario):
     enviador = Enviador()
 
@@ -18,3 +22,19 @@ def test_remetente(destinatario):
         'Turma henrique bastos'
     )
     assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'pessoasnil', ]
+)
+def test_remetente_ivalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'snil.pessoa@hotmail.com',
+            'Cursos de python',
+            'Turma henrique bastos'
+        )
+
