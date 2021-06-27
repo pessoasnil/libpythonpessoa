4eb11cca34bc3f6042324a64762d07bbad3b3321
pessoasnil <Metroide171190y@>
2021-06-27 15:54:31 -0300

    Fim python tolls

--- libpythonpessoa/tests/test_spam/test_github_api.py
+++ libpythonpessoa/tests/test_spam/test_github_api.py
@@ -5,7 +5,7 @@ import pytest
 from libpythonpessoa import github_api
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/63809250?v=4'
     resp_mock.json.return_value = {
@@ -13,10 +13,10 @@ def avatar_url():
         'node_id': 'MDQ6VXNlcjY3MzI5Nw==',
         'avatar_url': url,
     }
-    get_original = github_api.request.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock=mocker.patch('libpythonpessoa.github_api.requests.get')
+    get_mock.return_value=resp_mock
+    return url
+
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('pessoasnil')
