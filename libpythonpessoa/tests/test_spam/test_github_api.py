b09476a9bea8291c3a816b7dd48fe953d28af4b9
pessoasnil <Metroide171190y@>
2021-06-27 15:42:20 -0300

    isolamento de imports

--- libpythonpessoa/tests/test_spam/test_github_api.py
+++ libpythonpessoa/tests/test_spam/test_github_api.py
@@ -1,15 +1,33 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpessoa import github_api
 
-
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
-    resp_mock.json.return_value={'login': 'Lins', 'id': 673297,
-                                 'node_id': 'MDQ6VXNlcjY3MzI5Nw==',
-                                 'avatar_url': 'https://avatars.githubusercontent.com/u/673297?v=4',
+    url = 'https://avatars.githubusercontent.com/u/63809250?v=4'
+    resp_mock.json.return_value = {
+        'login': 'Lins', 'id': 673297,
+        'node_id': 'MDQ6VXNlcjY3MzI5Nw==',
+        'avatar_url': url,
     }
-
+    get_original = github_api.request.get
     github_api.requests.get = Mock(return_value=resp_mock)
-    url = github_api.buscar_avatar('Lins')
-    assert 'https://avatars.githubusercontent.com/u/673297?v=4' == url
+    yield url
+    github_api.requests.get = get_original
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('pessoasnil')
+    assert avatar_url == url
+
+
+
+
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('pessoasnil')
+    assert 'https://avatars.githubusercontent.com/u/63809250?v=4' == url
+
